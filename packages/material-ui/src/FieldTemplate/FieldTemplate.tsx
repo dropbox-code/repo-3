43d2a3aacc5c2385669ab62b89dee7b4e3ba5f9d
EssiSupponen <essi.supponen@visma.com>
2022-03-24 09:20:05 +0200

    KEHFORM-787: Add indentation when element is filtered

--- packages/material-ui/src/FieldTemplate/FieldTemplate.tsx
+++ packages/material-ui/src/FieldTemplate/FieldTemplate.tsx
@@ -8,6 +8,7 @@ import FormHelperText from "@material-ui/core/FormHelperText";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
+import Box from '@material-ui/core/Box';
 
 import WrapIfAdditional from "./WrapIfAdditional";
 
@@ -26,6 +27,15 @@ const getLabel = (uiSchema: any, defaultLabel: string) => {
     return useLabel ? label : title;
   }
   return defaultLabel;
+}
+
+const indentation = (element: {filter?: {show?: any}}) => {
+  console.log(element.filter);
+  if (element.filter && element.filter!.show) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const FieldTemplate = ({
@@ -52,53 +62,55 @@ const FieldTemplate = ({
   }
 
   return (
-    <WrapIfAdditional
-      classNames={classNames}
-      disabled={disabled}
-      id={id}
-      label={label}
-      onDropPropertyClick={onDropPropertyClick}
-      onKeyChange={onKeyChange}
-      readonly={readonly}
-      required={required}
-      schema={schema}>
-      <FormControl
-        fullWidth={true}
-        error={rawErrors.length ? true : false}
-        required={required}>
-        {showTitle(schema, uiSchema) ?
-          <Typography
-            aria-label={required ? intl.formatMessage({defaultMessage: 'Required field'}) : undefined}
-            variant="subtitle1">
-            { getLabel(uiSchema, label) }
-            { required ? ' *' : null }
-          </Typography>
-          : null}
-        {displayLabel && rawDescription && (schema.type !== 'boolean' || uiSchema['ui:widget'] === 'radio') ? (
-          <Typography
-            id={utils.descriptionId(id)}
-            variant="caption"
-            color="textSecondary">
-            {rawDescription}
-          </Typography>
-        ) : null}
-        {children}
-        {rawErrors.length > 0 && (
-          <List id={utils.errorsId(id)} dense={true} disablePadding={true}>
-            {rawErrors.map((error, i: number) => {
-              return (
-                <ListItem key={i} disableGutters={true}>
-                  <FormHelperText>{error}</FormHelperText>
-                </ListItem>
-              );
-            })}
-          </List>
-        )}
-        {rawHelp && (
-          <FormHelperText id={utils.helpId(id)}>{rawHelp}</FormHelperText>
-        )}
-      </FormControl>
-    </WrapIfAdditional>
+    <Box pl={uiSchema['ui:options'] && indentation(uiSchema['ui:options']!.element as {filter?: {show?: any}}) ? 3 : 0}>
+      <WrapIfAdditional
+        classNames={classNames}
+        disabled={disabled}
+        id={id}
+        label={label}
+        onDropPropertyClick={onDropPropertyClick}
+        onKeyChange={onKeyChange}
+        readonly={readonly}
+        required={required}
+        schema={schema}>
+        <FormControl
+          fullWidth={true}
+          error={rawErrors.length ? true : false}
+          required={required}>
+          {showTitle(schema, uiSchema) ?
+            <Typography
+              aria-label={required ? intl.formatMessage({defaultMessage: 'Required field'}) : undefined}
+              variant="subtitle1">
+              { getLabel(uiSchema, label) }
+              { required ? ' *' : null }
+            </Typography>
+            : null}
+          {displayLabel && rawDescription && (schema.type !== 'boolean' || uiSchema['ui:widget'] === 'radio') ? (
+            <Typography
+              id={utils.descriptionId(id)}
+              variant="caption"
+              color="textSecondary">
+              {rawDescription}
+            </Typography>
+          ) : null}
+          {children}
+          {rawErrors.length > 0 && (
+            <List id={utils.errorsId(id)} dense={true} disablePadding={true}>
+              {rawErrors.map((error, i: number) => {
+                return (
+                  <ListItem key={i} disableGutters={true}>
+                    <FormHelperText>{error}</FormHelperText>
+                  </ListItem>
+                );
+              })}
+            </List>
+          )}
+          {rawHelp && (
+            <FormHelperText id={utils.helpId(id)}>{rawHelp}</FormHelperText>
+          )}
+        </FormControl>
+      </WrapIfAdditional>
+    </Box>
   );
 };
 
