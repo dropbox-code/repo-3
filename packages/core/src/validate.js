baa549915db692f3604006694a2f3ec2f7ea08aa
EssiSupponen <essi.supponen@visma.com>
2022-06-15 15:23:49 +0300

    KEHFORM-941: Add formgroup validation

--- packages/core/src/validate.js
+++ packages/core/src/validate.js
@@ -213,20 +213,31 @@ export default function validateFormData(
     formerCustomFormats = customFormats;
   }
 
-  for (const key in schema.properties) {
-    if (
-      !schema.required.includes(key) &&
-      schema.properties[key].type === "array" &&
-      schema.properties[key].items.type !== "object" &&
-      !schema.properties[key].items.pattern &&
-      !schema.properties[key].items.minimum &&
-      !schema.properties[key].items.maximum &&
-      !schema.properties[key].items.maxLength
-    ) {
-      // do not validate these fields
-      delete formData[key];
+  const removeDataFromValidation = (elements, required, formData) => {
+    for (const key in elements) {
+      const element = elements[key];
+      if (element.type === "object") {
+        removeDataFromValidation(
+          element.properties,
+          element.required,
+          formData[key]
+        );
+      } else if (
+        !required.includes(key) &&
+        element.type === "array" &&
+        element.items.type !== "object" &&
+        !element.items.pattern &&
+        !element.items.minimum &&
+        !element.items.maximum &&
+        !element.items.maxLength
+      ) {
+        // do not validate these fields
+        delete formData[key];
+      }
     }
-  }
+  };
+
+  removeDataFromValidation(schema.properties, schema.required, formData);
 
   let validationError = null;
   try {
