409b17519fcadd820c322772f97ce6f002732a1f
EssiSupponen <essi.supponen@visma.com>
2022-12-14 15:48:46 +0200

    KEHFORM-1187: Add score

--- packages/material-ui/src/RadioWidget/RadioWidget.tsx
+++ packages/material-ui/src/RadioWidget/RadioWidget.tsx
@@ -4,9 +4,18 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
 
 import { WidgetProps } from "@visma/rjsf-core";
 import { FormHelperText } from '@material-ui/core';
+
+const getScore = (choices: {enumNames: string, enum?: string, meta?: {score: number}}[], value: string) => {
+  let choice = choices.find(choice => choice.enum === value);
+  if (!choice) {
+    choice = choices[Number(value)];
+  }
+  return choice && choice.meta ? choice.meta.score : 0;
+}
 
 const RadioWidget = ({
   id,
@@ -31,35 +40,48 @@ const RadioWidget = ({
   }: React.FocusEvent<HTMLInputElement>) => onFocus(id, value);
 
   const row = options ? options.inline : false;
+  const showScore = options ? options.showScore : false;
+  const rawChoices = options && options!.element ?
+    (options!.element! as {choices: {enumNames: string, enum?: string, meta?: {score: number}}[]}).choices :
+    [];
 
   return (
-    <FormControl component="fieldset">
-      <legend style={{position: 'absolute', clip: 'rect(0,0,0,0)'}}>{label}</legend>
-      {schema.type === 'boolean' && <FormHelperText component="span" >{options.description}</FormHelperText>}
-      <RadioGroup
-        value={`${value}`}
-        row={row as boolean}
-        onChange={_onChange}
-        onBlur={_onBlur}
-        onFocus={_onFocus}>
-        {(enumOptions as any).map((option: any, i: number) => {
-          const itemDisabled =
-            enumDisabled && (enumDisabled as any).indexOf(option.value) != -1;
+    <div style={{display: 'flex'}}>
+      <div style={{flex: '1 1'}}>
+        <FormControl component="fieldset">
+          <legend style={{position: 'absolute', clip: 'rect(0,0,0,0)'}}>{label}</legend>
+          {schema.type === 'boolean' && <FormHelperText component="span" >{options.description}</FormHelperText>}
+          <RadioGroup
+            value={`${value}`}
+            row={row as boolean}
+            onChange={_onChange}
+            onBlur={_onBlur}
+            onFocus={_onFocus}>
+            {(enumOptions as any).map((option: any, i: number) => {
+              const itemDisabled =
+                enumDisabled && (enumDisabled as any).indexOf(option.value) != -1;
 
-          const radio = (
-            <FormControlLabel
-              control={<Radio color="primary" key={i} />}
-              label={`${option.label}`}
-              value={`${option.value}`}
-              key={i}
-              disabled={disabled || itemDisabled || readonly}
-            />
-          );
+              const radio = (
+                <FormControlLabel
+                  control={<Radio color="primary" key={i} />}
+                  label={`${option.label}`}
+                  value={`${option.value}`}
+                  key={i}
+                  disabled={disabled || itemDisabled || readonly}
+                />
+              );
 
-          return radio;
-        })}
-      </RadioGroup>
-    </FormControl>
+              return radio;
+            })}
+          </RadioGroup>
+        </FormControl>
+      </div>
+      { (showScore && value) &&
+        <Typography>{
+          getScore(rawChoices, value)
+        }</Typography>
+      }
+    </div>
   );
 };
 
